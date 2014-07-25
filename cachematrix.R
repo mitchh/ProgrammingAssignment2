38d34ed06a779b5e878aa0ff94cff8bb86fed064
Mitch Hendrickson <mitch@vocelka.org>
2014-07-24 23:58:22 -0700

    Just comments, for a start.
    (grading is going to want one or more commits; might as well have more than one

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Programming assingment 2 for https://class.coursera.org/rprog-005
+# Mitch Hendrickson
 
-## Write a short comment describing this function
+# Per the assignment, we write a couple of functions to demonstrate/duplicate 
+# the vector caching described in the assignemnt, but this time for matrices
+
+## Create a "matrix" which is really a list of operation functions and has a place to 
+## stash a cache of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## Inverse finder for out casching matrix which will use the cached version if it can
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
